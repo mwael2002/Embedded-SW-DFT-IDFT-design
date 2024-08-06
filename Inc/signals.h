07d57b56a39718a65686d2565bca3964b6275121
Mohammad Wael <102371006+mwael2002@users.noreply.github.com>
2024-08-06 19:11:05 +0300

    Add files via upload

--- Inc/signals.h
+++ Inc/signals.h
@@ -9,7 +9,7 @@
 #define SIGNALS_H_
 
 #define LENGTH_SIGNAL_5HZ           301
-#define LENGTH_SIGNAL_1KHZ_32KHZ      320
+#define LENGTH_SIGNAL_1KHZ_15KHZ    320
 #define LENGTH_IMPULSE_RESPONSE     29
 #define LENGTH_SIGNAL_ECG           640
 
